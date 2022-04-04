7f3a8228e43a1ee44a69919551b77965ded7f4b0
Wong Sai Khong <saikhong.wong@aquariux.com>
2022-04-04 12:07:43 +0800

    feat: add simple test cases for checking view controller and search API

--- electrolux flickrTests/electrolux_flickrTests.swift
+++ electrolux flickrTests/electrolux_flickrTests.swift
@@ -9,7 +9,29 @@ import XCTest
 @testable import electrolux_flickr
 
 class electrolux_flickrTests: XCTestCase {
-
+    
+    var vc: EFImageListViewController!
+    
+    override func setUp() {
+        super.setUp()
+        vc = EFImageListViewController()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    func testViewController_not_nil() {
+        XCTAssertNotNil(vc, "EFImageListViewController instance should not be nil")
+    }
+    
+    func testSearchAPI() throws {
+        // When
+        vc.setupViewModelForUIUpdate()
+        // Then
+        XCTAssertTrue(true, "search API called")
+    }
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
